995ea930401b7ddee4998e941bfe919badafc196
tunggg <tranhuutungkeu@gmail.com>
2023-09-06 18:14:08 +0700

    update-reponsive

---
+++ assets/js/functions.js
@@ -0,0 +1,11 @@
+const dropdown = document.querySelector('.dropdown');
+
+dropdown.addEventListener('click', () => {
+    dropdown.classList.toggle('active')
+});
+
+const dropdown2 = document.querySelector('.dropdown2');
+
+dropdown2.addEventListener('click', () => {
+    dropdown2.classList.toggle('active')
+});
